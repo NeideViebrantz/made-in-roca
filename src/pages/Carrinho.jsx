c03e1217703d331f23206cd52b80f035720e43b6
greicibrandt <neideviebrantz@hotmail.com>
2026-09-13 18:48:28 -0300

    adiciona logo Made in Roça

--- src/pages/Carrinho.jsx
+++ src/pages/Carrinho.jsx
@@ -1,29 +1,70 @@
+
 import { Link } from "react-router-dom";
 import produtos from "../data/produtos";
 import ItemCarrinho from "../components/ItemCarrinho";
 import ResumoCompra from "../components/ResumoCompra";
+import logo from "../assets/img/logo-made-in-roca.png";
 
 function Carrinho() {
   return (
-    <main>
-      <h1>Meu Carrinho</h1>
-
-      <section>
-        {produtos.map((produto) => (
-          <ItemCarrinho
-            key={produto.id}
-            produto={produto}
+    <>
+      <header className="cabecalho">
+        <div className="logo">
+          <img
+            src={logo}
+            alt="Logo da loja Made in Roça"
           />
-        ))}
-      </section>
+        </div>
 
-      <ResumoCompra produtos={produtos} />
+        <nav>
+          <Link to="/">Início</Link>
+          <Link to="/">Produtos</Link>
+          <Link to="/">Carrinho</Link>
+          <Link to="/pagamento">Pagamento</Link>
+        </nav>
+      </header>
 
-      <Link to="/pagamento">
-        <button type="button">Ir para pagamento</button>
-      </Link>
-    </main>
+      <main>
+        <h2 className="titulo-carrinho">🛒 MEU CARRINHO</h2>
+
+        <section className="lista-produtos">
+          {produtos.map((produto) => (
+            <ItemCarrinho
+              key={produto.id}
+              produto={produto}
+            />
+          ))}
+        </section>
+
+        <ResumoCompra produtos={produtos} />
+
+        <Link className="botao-pagamento" to="/pagamento">
+          🛒 IR PARA PAGAMENTO →
+        </Link>
+      </main>
+
+      <footer className="rodape">
+        <div>
+          🚚
+          <strong> ENTREGA SEGURA</strong>
+          <span>Para todo o Brasil</span>
+        </div>
+
+        <div>
+          🛡️
+          <strong> COMPRA PROTEGIDA</strong>
+          <span>Seus dados em segurança</span>
+        </div>
+
+        <div>
+          🐴
+          <strong> QUALIDADE GARANTIDA</strong>
+          <span>Produtos selecionados com carinho</span>
+        </div>
+      </footer>
+    </>
   );
 }
 
 export default Carrinho;
+
